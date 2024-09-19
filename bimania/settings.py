8727b5767bbe56fb1545ebd4dd7d0e08fb80e518
Andrew Butler <sephbin@gmail.com>
2024-09-19 17:34:35 +1000

    Update settings.py

--- bimania/settings.py
+++ bimania/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-46owpm-qk=jyf8re+m*)zw_^ca%2l+p!9lyzv3sw_vymbt9r4b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
