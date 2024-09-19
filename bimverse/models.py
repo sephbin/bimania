5849d352010219de4277255a91444cd30310cb33
Andrew Butler <sephbin@gmail.com>
2024-09-19 17:50:52 +1000

    edgeObject, nodeObject

--- bimverse/models.py
+++ bimverse/models.py
@@ -2,18 +2,10 @@ from django.db import models
 from .models_utils import *
 
 
-class Category(parentModel):
-    name = models.CharField(max_length=100)
-
+class edgeObject(parentModel):
     def __str__(self):
         return self.name
 
-class Ingredient(parentModel):
-    name = models.CharField(max_length=100)
-    notes = models.TextField()
-    category = models.ForeignKey(
-        Category, related_name="ingredients", on_delete=models.CASCADE
-    )
-
+class nodeObject(parentModel):
     def __str__(self):
         return self.name
