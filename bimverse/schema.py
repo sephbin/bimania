a47b8f49d254d88aca6e4430eb3e4afb4b6cc119
Andrew Butler <sephbin@gmail.com>
2024-09-19 18:01:29 +1000

    Update schema.py

--- bimverse/schema.py
+++ bimverse/schema.py
@@ -1,30 +1,31 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Category, Ingredient
+from .models import *
 
-class CategoryType(DjangoObjectType):
+class edgeObject_Type(DjangoObjectType):
     class Meta:
-        model = Category
-        fields = ("id", "name", "ingredients")
+        model = edgeObject
+        fields = ("identifier", "name", "enabled", "hasError")
 
-class IngredientType(DjangoObjectType):
+class nodeObject_Type(DjangoObjectType):
     class Meta:
-        model = Ingredient
-        fields = ("id", "name", "notes", "category")
+        model = nodeObject
+        fields = ("identifier", "name", "enabled", "hasError")
 
 class Query(graphene.ObjectType):
-    all_ingredients = graphene.List(IngredientType)
-    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
+    all_node_objects = graphene.List(nodeObject_Type)
+    edges_by_name = graphene.Field(edgeObject_Type, name=graphene.String(required=True))
 
-    def resolve_all_ingredients(root, info):
+    def resolve_all_node_objects(root, info):
         # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related("category").all()
+        # return nodeObject.objects.select_related("edgeObject").all()
+        return nodeObject.objects.all()
 
-    def resolve_category_by_name(root, info, name):
+    def resolve_edges_by_name(root, info, name):
         try:
-            return Category.objects.get(name=name)
-        except Category.DoesNotExist:
+            return edgeObject.objects.get(name=name)
+        except edgeObject.DoesNotExist:
             return None
 
 schema = graphene.Schema(query=Query)
