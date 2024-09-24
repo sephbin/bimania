dad271b9339d849127deeaf67d4d18d8f8b7a88e
Andrew Butler <sephbin@gmail.com>
2024-09-24 16:17:26 +1000

    modularClasses Slug

--- rhino/process Data.py
+++ rhino/process Data.py
@@ -1,13 +1,24 @@
 import requests
-
-data = requests.get("https://archmanucox.pythonanywhere.com/bimverse/nodes-light.json/?search=Floor Slab").json()
+import json
+server = "http://localhost:8000"
+#server = "https://archmanucox.pythonanywhere.com"
+data = requests.get("%s/bimverse/nodes-light.json/?search=FloorSlab"%(server)).json()
 
 # print(data)
 
+tag = "floor"
+tagIndex = requests.get("{}/bimverse/modularClasses.json/?search={}".format(server,tag)).json()[0]["id"]
+
+
 for d in data:
-	if "floor" not in d["modularClassTags"]:
-		# print(d)
-		d["modularClassTags"] = d["modularClassTags"]+["floor"]
-		requests.put("https://archmanucox.pythonanywhere.com/bimverse/nodes-light/{id}".format(**d), data=d)
+	if tag not in d["modularClassTags"]:
 		print(d)
+		d["modularClassTags"] = d["modularClassTags"]+[tag]
+		d["data"] = json.dumps(d["data"])
+		d["server"] = server
+		put = requests.put("{server}/bimverse/nodes-light/{id}/".format(**d), data=d)
+		print(put.status_code)
+		print(put.reason)
+		with open("result.html","w") as file:
+			file.write(put.text)
 		break
